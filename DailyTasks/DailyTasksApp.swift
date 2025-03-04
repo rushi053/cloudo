f516db11ad4fa86c125b00bef44d1b15eea92e5e
rushi <rjadeja053@gmail.com>
2025-03-04 12:27:26 +0530

    Initial Commit

---
+++ DailyTasks/DailyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTasksApp.swift
+//  DailyTasks
+//
+//  Created by Rushiraj Jadeja on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyTasksTests/DailyTasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTasksTests.swift
+//  DailyTasksTests
+//
+//  Created by Rushiraj Jadeja on 04/03/25.
+//
+
+import Testing
+@testable import DailyTasks
+
+struct DailyTasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
