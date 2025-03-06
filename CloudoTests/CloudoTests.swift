d42dbed8cba3c04b676ed413d3e8b5f1abf7ea36
rushi <rjadeja053@gmail.com>
2025-03-06 23:13:59 +0530

    Initial Commit

---
+++ CloudoTests/CloudoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloudoTests.swift
+//  CloudoTests
+//
+//  Created by Rushiraj Jadeja on 06/03/25.
+//
+
+import Testing
+@testable import Cloudo
+
+struct CloudoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
